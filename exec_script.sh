f80bc9cd2b535b89b05704e1508f891d5e7d0155
vitaglianog <vitaglianog@gmail.com>
2025-06-02 16:11:17 -0400

    Merge

--- exec_script.sh
+++ exec_script.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
 for workload in "archeology" "astronomy" "biomedical" "legal" "environment" "wildfire"; do
-for sut_name in "BaselineLLMSystemDeepseekR1" "BaselineLLMSystemGPTo3" "BaselineLLMSystemGPT4o" "BaselineLLMSystemQwen2_5Coder" BaselineLLMSystemLlama3_3Instruct;  do
-for config in "Naive" "OneShot" "FewShot"; do
-    # echo "Running workload: $workload with SUT: $sut_name$config"
-    screen -d -m python evaluate.py --sut $sut_name$config --workload $workload.json --dataset_name $workload
+for sut_name in "DeepseekR1" "Llama3_3Instruct" "Qwen2_5Coder" "GPT4o" "GPTo3"; do
+ for config in "Naive" "OneShot" "FewShot"; do
+#  for config in "Naive"; do
+    echo "Running workload: $workload with SUT: $sut_name$config"
+    python evaluate.py --sut BaselineLLMSystem$sut_name$config --workload $workload.json --dataset_name $workload --use_system_cache
 done
 done
 done
