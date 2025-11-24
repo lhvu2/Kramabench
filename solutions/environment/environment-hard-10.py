4e81c65b0e0cac66a388c879ed6870739bd4b93d
ey-l <yujinglai0707@gmail.com>
2025-11-24 18:20:26 -0500

    pull from main

--- solutions/environment/environment-hard-10.py
+++ solutions/environment/environment-hard-10.py
@@ -59,11 +59,49 @@ df1 = df[df['Community'].isin(more_than_90_cities)]
 df1["Beach Name"] = df1['Beach Name'].str.split('@').str[0]
 df1['Beach Name'].unique()
 
-beach_name = 'wollaston_beach_datasheet.csv'
-df = prepare_beach_datasheet(os.path.join(data_path, beach_name))
-# Impute missing values in '1-Day Rain', '2-Day Rain', '3-Day Rain', 'Enterococcus'
-df['Total Rain'] = df['1-Day Rain'] + df['2-Day Rain'] + df['3-Day Rain']
-### NOTE: not sure if 3-Day Rain is the rain on Day 3 or the total rain over 3 days
+boston_beach_names = ['constitution_beach_datasheet.csv', 'pleasure_bay_and_castle_island_beach_datasheet.csv', 
+                    'city_point_beach_datasheet.csv', 'm_street_beach_datasheet.csv', 'carson_beach_datasheet.csv', 
+                    'malibu_beach_datasheet.csv', 'tenean_beach_datasheet.csv', 'wollaston_beach_datasheet.csv']
+# Get the beach names from boston_beach_names without the '_datasheet.csv' suffix and with spaces instead of underscores, and title case
+formatted_boston_beach_names = [name.replace('_beach_datasheet.csv', '') for name in boston_beach_names]
+# If and in the beach name, split by 'and' and keep both parts
+final_boston_beach_names = []
+for name in formatted_boston_beach_names:
+    if 'and' in name:
+        parts = name.split('_and_')
+        for part in parts:
+            final_boston_beach_names.append(part.replace('_', ' ').title())
+    else:
+        final_boston_beach_names.append(name.replace('_', ' ').title())
+print(f"Boston beach names: {final_boston_beach_names}")
+
+# Take a union of the beach names in df1 and final_boston_beach_names
+df1_beach_names = df1['Beach Name'].str.title().unique().tolist()
+# strip whitespace from beach names in df1_beach_names
+df1_beach_names = [name.strip() for name in df1_beach_names]
+common_beaches = set(df1_beach_names).intersection(set(final_boston_beach_names))
+print(f"Common beaches between EJ >90% communities and Boston beaches: {common_beaches}")
+
+# Find the beach datasheet for common_beaches
+target_beach_files = []
+for beach in common_beaches:
+    beach_file_name = beach.lower()
+    for f in boston_beach_names:
+        if beach_file_name in f:
+            target_beach_files.append((beach, f))
+            break
+
+print(f"Target beach files: {target_beach_files}")
+
+dfs = []
+# For each target beach file, get the dataframe, and union them
+for beach, beach_file in target_beach_files:
+    df = prepare_beach_datasheet(os.path.join(data_path, beach_file))
+    # Impute missing values in '1-Day Rain', '2-Day Rain', '3-Day Rain', 'Enterococcus'
+    dfs.append(df)
+
+df = pd.concat(dfs, ignore_index=True)
+
 # Compute the correlation between 'Total Rain' and 'Enterococcus'
 correlation = df['3-Day Rain'].corr(df['Enterococcus'], method='pearson')
 print(f"Correlation between Total Rain and Enterococcus: {correlation:.3f}")
